8525b0e9e669e05baf61a3a83076196e0d90b2cc
Mahmoud Ashraf <mahmoudss1997@gmail.com>
2019-07-31 17:13:10 +0200

    Mashraf/word sync (#58)
    * migrate to the new transcribe api
    
    * :lipstick: fix words styling
    
    * :zap: render the ayah once the transcriber find it
    
    * fix how to deal with word count
    
    * setup default mode
    
    * show partial query until ayah found emit
    
    * refactor the code and add translation
    
    * create function to connect to transcribe socket and fix reset button function
    
    * fix word by word syncing
    
    * :pencil2: fix formatWords typo
    
    * :pencil2: writing docs
    
    * :bug: fix if the symbol first word

--- src/pages/Transcribe/TranscribeAyah.tsx
+++ src/pages/Transcribe/TranscribeAyah.tsx
@@ -26,15 +26,87 @@ const Word = Styled.span`
 	color: ${(props: IWordProps) => (props.color ? props.color : colors.black)}
 `;
 
-interface IState {
+interface IProps {
   ayah: IAyahShape;
   isTranscribed: boolean;
   currentTranscribedIndex?: number;
 }
 
-class TranscribeAyah extends React.Component<IState> {
-  renderWords = () =>
-    this.props.ayah.words.map(
+interface IState {
+  words: WordShape[];
+}
+
+class TranscribeAyah extends React.Component<IProps, IState> {
+  state = {
+    words: this.props.ayah.words,
+  };
+
+  /*
+   * foramteWords is a helper function to merge symbol words e.g: 'pause words' into the next or prev word
+   * e.g:
+   * the words before format
+   * words: [..., { ..., code: "&#xfb58;", textSimple: "الارض"  }, { ..., code: "&#xfb59;", textMadani: null  }, ...]
+   * after format the words array
+   * formatedWords: [ ..., { ..., code: "&#xfb58; &#xfb59;", textSimple: "الارض"  }, ...  ]
+   */
+  formatWords = () => {
+    const { words } = this.props.ayah;
+
+    // filter the words of type 'word' or type 'end'
+    const wordsOfTypeWordAndEnd = words.filter(
+      word =>
+        word.charType === WORD_TYPES.CHAR_TYPE_WORD ||
+        word.charType === WORD_TYPES.CHAR_TYPE_END
+    );
+
+    // filter the symbol words like 'pause, sajda'
+    const symbolWords = words.filter(
+      word =>
+        word.charType !== WORD_TYPES.CHAR_TYPE_WORD &&
+        word.charType !== WORD_TYPES.CHAR_TYPE_END
+    );
+
+    // reduce the words array into a modified one
+    const modifidedWords = symbolWords.reduce((prev, word, index) => {
+      // get the index of the word that we will merge the symbol on it
+      const validWordIndex = prev.findIndex(
+        w => w.id === word.id - 1 || w.id === word.id + 1
+      );
+
+      const validWord = prev[validWordIndex];
+      // check if the valid word index greater than symbol index so we should add symbol code before it's code, vice versa
+      const code =
+        validWordIndex > index
+          ? `${validWord.code} ${word.code}`
+          : `${word.code} ${validWord.code}`;
+      // return the new array by modify the word by join the symbol code with the word code
+      return [
+        ...prev.slice(0, validWordIndex),
+        {
+          ...validWord,
+          code,
+        },
+        ...prev.slice(validWordIndex + 1),
+      ];
+    }, wordsOfTypeWordAndEnd);
+
+    this.setState({
+      words: modifidedWords ? modifidedWords : wordsOfTypeWordAndEnd,
+    });
+  };
+
+  componentDidMount() {
+    this.formatWords();
+  }
+
+  componentDidUpdate(prevProps: IProps, prevState: IState) {
+    if (this.props.ayah.id !== prevProps.ayah.id) {
+      this.formatWords();
+    }
+  }
+
+  renderWords = () => {
+    return this.state.words.map(
       ({ charType, className, code, id }: WordShape, index: number) => {
         const classname = classNames({
           [className]: true,
@@ -66,6 +138,7 @@ class TranscribeAyah extends React.Component<IState> {
         );
       }
     );
+  };
 
   render() {
     return <VerseWrapper>{this.renderWords()}</VerseWrapper>;
